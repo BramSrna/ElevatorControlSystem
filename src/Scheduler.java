1996cdfedfe492598181f79f2ca666f8198a852e
Haseeb <haseeb01001@gmail.com>
2019-01-21 17:41:14 -0500

    Started working on Scheduler

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -3,6 +3,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Scheduler {
@@ -29,7 +31,7 @@ public class Scheduler {
 	 * HOW TO DECODE MESSAGES:
 	 * 
 	 * <pre>
-	 * []
+	 * [MODE, MODE, MODE, MESSAGE, MESSAGE, MESSAGE]
 	 * </pre>
 	 */
 
@@ -39,6 +41,9 @@ public class Scheduler {
 	private final int MAX_BYTE_ARRAY_SIZE = 100;
 	private int sendPortNum;
 	private List<Integer> floorsToVisit;
+	private final byte[] FLOOR_BUTTON_HIT_MODE = { 1, 2, 3 };
+	private final byte[] REACHED_FLOOR_MESSAGE = { 4, 5, 6 };
+	private final byte[] ERROR_DECODING_MESSAGE = { 83, 79, 83 };
 
 	private Scheduler() {
 		floorsToVisit = new ArrayList<Integer>();
@@ -91,10 +96,67 @@ public class Scheduler {
 		sendSocket.close();
 	}
 
+	/**
+	 * Decode the message sent in order to create the proper response message.
+	 * 
+	 * @param dataPacket Data send to the Scheduler.
+	 * @return
+	 */
 	private byte[] decodeMessageAndGetResponse(DatagramPacket dataPacket) {
+		byte[] recievedData = dataPacket.getData();
+		byte[] mode = { recievedData[0], recievedData[1], recievedData[2] };
 		// TODO
-		byte[] responseByte = new byte[MAX_BYTE_ARRAY_SIZE];
-		return responseByte;
+		if (mode.equals(FLOOR_BUTTON_HIT_MODE)) {
+			return extractDataForFloorButtonHitAndGenerateResponse(recievedData);
+		} else if (mode.equals(REACHED_FLOOR_MESSAGE)) {
+			return extractDataForReachingFloorAndGenerateResponse(recievedData);
+		}
+
+		return ERROR_DECODING_MESSAGE;
+	}
+
+	private byte[] extractDataForFloorButtonHitAndGenerateResponse(byte[] recievedData) {
+		// TODO
+		int buttonPressed = 0;
+		floorButtonPressed(buttonPressed);
+		return recievedData;
+	}
+
+	/**
+	 * This method gets the details of the message in the case that the mode of the
+	 * message was that it reached a certain floor.
+	 * 
+	 * @param recievedData Data containing the details of the message
+	 * @return Response message
+	 */
+	private byte[] extractDataForReachingFloorAndGenerateResponse(byte[] recievedData) {
+		// TODO
+		int currentFloor = 0;
+		reachedFloor(currentFloor);
+		return recievedData;
+	}
+
+	/**
+	 * If a floor button in the elevator is hit and it is not already in the
+	 * floorsToVisit list, then add it to the list and organize the list
+	 * numerically.
+	 * 
+	 * @param floor The floor button pressed in the elevator.
+	 */
+	private void floorButtonPressed(int floor) {
+		if (!floorsToVisit.contains(floor)) {
+			floorsToVisit.add(floor);
+			Collections.sort(floorsToVisit);
+		}
+	}
+
+	/**
+	 * If the elevator has arrived at a floor, remove it from the floors to visit.
+	 * 
+	 * @param floor The floor we arrived on.
+	 */
+	private void reachedFloor(int floor) {
+		floorsToVisit.removeAll(Arrays.asList(floor));
 	}
 
 }
