7a27eb5cda35c2b3dcfd029223ed54f77afaade0
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2019-01-25 14:29:24 -0500

    Add files via upload

--- elevatorSubsystem.java
+++ elevatorSubsystem.java
@@ -28,34 +28,27 @@ import java.util.*;
  * PORT Numbers:
  * Elevator port 69
  * 	Receiving from Scheduler on Port 420
- * 	Sending to ? on Port xxxx
  * 
- * MESSAGE ENCODING:
- * 	Bytes[x,y] = 
- * 
- * (Currently the class only carries simple server features).
- * Last Edited January 22,2019
+ * Last Edited January 25,2019
  * 
  */
 public class elevatorSubsystem {
 	DatagramPacket sendPacket, receivePacket;
 	DatagramSocket sendSocket, receiveSocket;
-	
-	//The current floor the elevator is on
+
+	// The current floor the elevator is on
 	private int elevatorNumber = 1;
 	private int currentFloor = 1;
 	private int numberOfElevators = 0;
 	private int numberOfFloors = 0;
-	
+
 	private lampState[] allButtons;
-	
-	
+
 	private enum lampState {
-        OFF,
-        ON
-    }
-	
-	// Motor only says go up or go down 
+		OFF, ON
+	}
+
+	// Motor only says go up or go down
 
 	public elevatorSubsystem() {
 		try {
@@ -76,102 +69,135 @@ public class elevatorSubsystem {
 			System.exit(1);
 		}
 	}
-	
+
 	public int getElevatorNumber() {
 		return this.elevatorNumber;
 	}
+
 	public int getCurrentFloor() {
 		return this.currentFloor;
 	}
-	
+
 	// Interior Elevator Display
 	public void display() {
 		System.out.println("Elevator " + this.getElevatorNumber());
 		System.out.println("Floor # " + this.getCurrentFloor());
-		//ideally we want lights lighting up by whatever floor it is, not sure how we wil implement that yet
+		// ideally we want lights lighting up by whatever floor it is, not sure how we
+		// wil implement that yet
 	}
 	
-	public void start() {
-		// start elevator until told to stop
+	// Start elevator until told to stop
+	public void startMotor() {
 		System.out.println("Elevator Started Moving");
 	}
-	public void stop() {
-		// stop the elevator
+
+	// Stop the elevator
+	public void stopMotor() {
 		System.out.println("Elevator Stopped Moving");
 	}
+	
+	// Open the elevator door
 	public void openDoor() {
 		System.out.println("Elevator Door Opened");
 	}
+	
+	// Close the elevator door
 	public void closeDoor() {
 		System.out.println("Elevator Door Closed");
 	}
 	
+	// called within exchangeData() method to perform required actions
 	public void performAction(String str, byte[] data) {
 		this.display();
-		if(str.equals("config")) {
+		if (str.equals("config")) {
 			numberOfElevators = data[1];
 			numberOfFloors = data[2];
-			
-			// for now this array is from 0 to num of Floors and encoded (on/off respectively), you can do it however u prefer
-			for(int i=0; i<numberOfFloors; i++) {
+
+			// for now this array is from 0 to num of Floors and encoded (on/off
+			// respectively), you can do it however u prefer
+			for (int i = 0; i < numberOfFloors; i++) {
 				allButtons[i] = lampState.OFF;
 			}
 		}
-		if(str.equals("start/stop")) {
+		if (str.equals("start/stop")) {
 			int destinationFloor = data[1]; // Go to this floor
 			currentFloor = data[2];
-			if(data[3]==0) {
-				this.start();//Start elevator
-			}else if(data[3]==1) {
-				this.stop();//Stop elevator
+			if (data[3] == 0) {
+				this.startMotor();// Start elevator
+			} else if (data[3] == 1) {
+				this.stopMotor();// Stop elevator
 			}
 		}
-		if(str.equals("open/close")) {
-			if(data[1]==0) {
-				this.closeDoor();//Start elevator
-			}else if(data[1]==1) {
-				this.openDoor();//Stop elevator
+		if (str.equals("open/close")) {
+			if (data[1] == 0) {
+				this.closeDoor();// Start elevator
+			} else if (data[1] == 1) {
+				this.openDoor();// Stop elevator
 			}
-		}if(str.equals("send clicked floor")) {
+		}
+		if (str.equals("send clicked floor")) {
 			// send clicked floor to scheduler
-		}else {
-			//invalid case
+			byte clickedFloor = 5;
+			byte[] clickedButton = { 3, clickedFloor, -1 };
+			try {
+				sendPacket = new DatagramPacket(clickedButton, clickedButton.length, InetAddress.getLocalHost(), 420);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			System.out.println("Elevator: Sending packet:");
+			System.out.println("To host: " + sendPacket.getAddress());
+			System.out.println("Destination host port: " + sendPacket.getPort());
+			int len = sendPacket.getLength();
+			System.out.println("Length: " + len);
+			System.out.print("Containing: ");
+			System.out.println(Arrays.toString(clickedButton)); // or could print "s"
+
+			try {
+				sendSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			System.out.println("Elevator: Packet sent.\n");
+			
+		} 
+		else {
+			// invalid case
 			System.out.println("Invalid Packet Format");
 			System.exit(1); // invalid
 		}
 	}
-	
+
 	// Method to validate the form of the array of bytes received from the Scheduler
 	public String validPacket(byte[] data) {
-		if(data[0]== 0) {
+		if (data[0] == 0) {
 			return "config";
-		}else if(data[0]==3) {
+		} else if (data[0] == 3) {
 			// send the number of floor clicked
 			return "send clicked floor";
-		}else if(data[0]== 4) {
+		} else if (data[0] == 4) {
 			return "start/stop";
-		}else if(data[0]==5) {
+		} else if (data[0] == 5) {
 			return "close/open";
 		}
 		return "invalid"; // anything else is an invalid request.
 	}
-	
+
 	public void exchangeData() {
 		// receive config layer out the elevators and the floors (once)
-		// receive command to move 
+		// receive command to move
 		// recieve command to control doors
-		
-		// Construct a DatagramPacket for receiving packets up
-		// to 100 bytes long (the length of the byte array).
-		
-		
+
 		// Receiving Data from the Scheduler to Control the Motor and Open the doors
-		this.display();
-		
+		this.display(); // automatically show display
+
 		byte data[] = new byte[100];
 		receivePacket = new DatagramPacket(data, data.length);
-		System.out.println("Elevater: Waiting for Packet.\n");
-		
+		System.out.println("Elevator: Waiting for Packet.\n");
+
 		// Block until a datagram packet is received from receiveSocket.
 		try {
 			System.out.println("Waiting..."); // so we know we're waiting
@@ -190,10 +216,7 @@ public class elevatorSubsystem {
 		int len = receivePacket.getLength();
 		System.out.println("Length: " + len);
 		System.out.print("Containing: ");
-
-		// Form a String from the byte array.
-		String received = new String(data, 0, len);
-		System.out.println(received + "\n");
+		System.out.println(Arrays.toString(data) + "\n");
 
 		// Slow things down (wait 5 seconds)
 		try {
@@ -202,20 +225,18 @@ public class elevatorSubsystem {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
-		String request = this.validPacket(data);
-		this.performAction(request, data); // do whatever needs to be done
-		
-		//Add delay
+
+		String request = this.validPacket(data); // returns the status of the packet information
+		this.performAction(request, data); // if the packet is valid, an action will be performed
 	}
 
 	public static void main(String args[]) {
+		//for now we only have one elevator
 		elevatorSubsystem elevator1 = new elevatorSubsystem();
-		for(;;) {
+		for (;;) {
 			// always wait for a message from the scheduler
 			elevator1.exchangeData();
 		}
-		
-		
+
 	}
 }
