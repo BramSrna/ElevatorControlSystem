f1ed6c9a78d9e817f070694f89d792f2b9c0dd56
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2019-02-01 11:38:34 -0500

    Add files via upload

--- Elevator.java
+++ Elevator.java
@@ -48,8 +48,14 @@ public class Elevator {
 	
 	// The destination floor
 	private int destinationFloor;
+	
+	
 
 	// USED ENUMS:
+	//Enum for Door
+	enum doorState {OPEN, CLOSED}
+	private doorState door = doorState.CLOSED;
+	
 	// Enum for all lights
 	enum lampState {OFF, ON}
 	// State machine states
@@ -107,7 +113,7 @@ public class Elevator {
 		System.out.println("Elevator " + this.getElevatorNumber());
 		System.out.println("Floor # " + this.getCurrentFloor());
 		for(int i=0; i<allButtons.length; i++) {
-			System.out.println("Floor Number " + i + ": " +allButtons[i]);
+			System.out.println("Floor Number " + i + ": " +allButtons[i] + ", Door: " + door);
 		}
 	}
 	/*
@@ -394,6 +400,7 @@ public class Elevator {
 			Thread.currentThread().interrupt();
 		}
 		System.out.println("Elevator Door Opened");
+		door = doorState.OPEN;
 	}
 
 	/*
@@ -406,6 +413,7 @@ public class Elevator {
 			Thread.currentThread().interrupt();
 		}
 		System.out.println("Elevator Door Closed");
+		door = doorState.CLOSED;
 	}
 	
 	
