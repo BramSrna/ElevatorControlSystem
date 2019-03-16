ad0b37f805e2cabfb854cc2c3011fc7897f0a87c
Haseeb <haseebakhan@cmail.carleton.ca>
2019-03-16 14:58:22 -0400

    Added Request file

---
+++ src/Request.java
@@ -0,0 +1,57 @@
+
+public class Request {
+
+	private long elevatorRequestTime;
+	private long elevatorPickupTime;
+	private long elevatorArrivedDestinationTime;
+	private boolean elevatorPickupTimeFlag;
+	private boolean elevatorArrivedDestinationTimeFlag;
+	private byte source;
+	private byte destination;
+
+	public Request(long requestArrived, byte source, byte destination) {
+		elevatorRequestTime = requestArrived;
+		this.source = source;
+		this.destination = destination;
+		elevatorPickupTime = 0;
+		elevatorArrivedDestinationTime = 0;
+		elevatorPickupTimeFlag = false;
+		elevatorArrivedDestinationTimeFlag = false;
+		// printRequestDetails();
+	}
+
+	public void setElevatorPickupTimeFlag() {
+		elevatorPickupTimeFlag = true;
+	}
+
+	public void setElevatorArrivedDestinationTimeFlag() {
+		elevatorArrivedDestinationTimeFlag = true;
+	}
+
+	public void setElevatorPickupTime(long time) {
+		elevatorPickupTime = time;
+		elevatorPickupTimeFlag = false;
+		printRequestDetails();
+	}
+
+	public void setElevatorArrivedDestinationTime(long time) {
+		elevatorArrivedDestinationTime = time;
+		elevatorArrivedDestinationTimeFlag = false;
+		printRequestDetails();
+	}
+
+	public void printRequestDetails() {
+		System.out.println("\nELEVATOR REQUEST: ");
+		System.out.println("Source: " + source + "Destination: " + destination);
+		System.out.println("Elevator was requested at: " + elevatorRequestTime + "ns.");
+		if (elevatorPickupTime != 0) {
+			System.out.println("It took " + (elevatorPickupTime - elevatorRequestTime)
+					+ "ns for an elevator to pickup the request.");
+		}
+		if (elevatorArrivedDestinationTime != 0) {
+			System.out.println("It took " + (elevatorArrivedDestinationTime - elevatorRequestTime)
+					+ "ns for the passenger to reach their destination from time of request.");
+		}
+	}
+
+}
